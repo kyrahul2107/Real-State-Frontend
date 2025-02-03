c0478d4974f02ca0147c4b433e295de1b69d0f2a
Rahul Kumar Yadav <rahulkumaryadav2107@gmail.com>
2025-02-03 23:04:49 +0530

    Merging this Branch

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
-
+// I wanted to merge this Branch to main
 function App() {
   return (
     <div className="App">
